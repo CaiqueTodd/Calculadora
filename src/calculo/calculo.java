7287968bc1df6f105e866402f5dc6cf8ccf4253e
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2022-12-28 23:48:11 -0300

    Add files via upload

---
+++ src/calculo/calculo.java
@@ -0,0 +1,21 @@
+package calculo;
+public class calculo {
+    
+    public double adicao(double v1, double v2){
+       return v1 + v2;
+    }
+    
+    public double subtracao(double v1, double v2) {
+        return v1 - v2;
+    }
+    
+    public double multiplicacao(double v1, double v2) {
+        return v1 * v2;
+    }
+    
+        public double divisao(double v1, double v2) {
+        return v1 / v2;
+    }
+    
+}
+
